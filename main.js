40d3bea150d98a62a37753f67c558e4d72cb8fe3
mark-cxfabric <118482809+mark-cxfabric@users.noreply.github.com>
2024-12-10 10:38:28 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+
+(function () {
+  var variables = {};
+
+  const actionfunction1 = async (params, credential) => {
+    // code logic goes here
+  }
+
+  return {
+    actionfunction1,
+  }
+})();
